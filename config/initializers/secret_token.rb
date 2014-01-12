0025c530f1e8be61ccef0c0ac82b200c5410fa48
renandeandradevaz <renanandrade_rj@hotmail.com>
2014-01-12 11:38:39 -0200

    sdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnaliseGastos::Application.config.secret_key_base = 'e355b19bf63e2864a6fbea1a0acbe9e3aac9c990fccd8e4a7796aafc52b3711f0ae440e131d9ae83ea3262912af303c936fa4093fec35388cf22b43ee33ed6af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnaliseGastos::Application.config.session_store :cookie_store, key: '_analise-gastos_session'
