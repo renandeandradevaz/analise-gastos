4ff84de13866a1335e881da8616d30a2bc2d841e
renandeandradevaz <renanandrade_rj@hotmail.com>
2014-06-02 22:19:30 -0300

    dfasfasf

--- app/controllers/contribuintes_controller.rb
+++ app/controllers/contribuintes_controller.rb
@@ -1,5 +1,5 @@
 class ContribuintesController < ApplicationController
-  before_action :set_contribuinte, only: [:show, :edit, :update, :destroy]
+  before_action :set_contribuinte, only: [:show]
 
   def index
     @contribuintes = Contribuinte.all
@@ -9,53 +9,8 @@ class ContribuintesController < ApplicationController
     session[:contribuinte_id] = @contribuinte.id
   end
 
-  def new
-    @contribuinte = Contribuinte.new
-  end
-
-  def edit
-  end
-
-  def create
-    @contribuinte = Contribuinte.new(contribuinte_params)
-
-    respond_to do |format|
-      if @contribuinte.save
-        format.html { redirect_to @contribuinte, notice: 'Contribuinte was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @contribuinte }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @contribuinte.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @contribuinte.update(contribuinte_params)
-        format.html { redirect_to @contribuinte, notice: 'Contribuinte was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @contribuinte.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @contribuinte.destroy
-    respond_to do |format|
-      format.html { redirect_to contribuintes_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
   def set_contribuinte
     @contribuinte = Contribuinte.find(params[:id])
-  end
-
-  def contribuinte_params
-    params.require(:contribuinte).permit(:nome)
   end
 end
--- app/controllers/gastos_controller.rb
+++ app/controllers/gastos_controller.rb
@@ -1,40 +1,17 @@
 class GastosController < ApplicationController
-  before_action :set_gasto, only: [:edit, :update, :destroy]
 
   def new
     @gasto = Gasto.new
   end
 
-  def edit
-  end
-
   def create
     @gasto = Gasto.new(gasto_params)
     @gasto.contribuinte = Contribuinte.find(session[:contribuinte_id])
-
     @gasto.save
-
-    redirect_to contribuinte_path @gasto.contribuinte
-
-  end
-
-  def update
-
-    @gasto.update(gasto_params)
-
-    redirect_to contribuinte_path @gasto.contribuinte
-  end
-
-  def destroy
-    @gasto.destroy
     redirect_to contribuinte_path @gasto.contribuinte
   end
 
   private
-  def set_gasto
-    @gasto = Gasto.find(params[:id])
-  end
-
   def gasto_params
     params.require(:gasto).permit(:valor, :descricao)
   end
